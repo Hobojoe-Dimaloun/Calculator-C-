7a0029397c34bd7f3d01e11b70899c4071aee3f5
Michael O'Donnell <mo14776@my.bristol.ac.uk>
2016-07-31 13:48:42 +0100

    header file

---
+++ src/string_parse.h
@@ -0,0 +1,13 @@
+#ifndef STRING_PARSE_H_
+#define STRING_PARSE_H_
+
+#include <stdio.h>
+
+double numberRead(char **inputString);
+
+int constantsLetterRead(char **inputString);
+
+int bannedLetterRead();
+
+
+#endif // STRING_PARSE_H_
